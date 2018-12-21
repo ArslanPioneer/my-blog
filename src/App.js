1ef54dae4429fe10f41f0bfb21126781645af8a4
ArslanPioneer <1041238415@qq.com>
2018-12-22 00:24:00 +0800

    react-router-dom 划分路由实现页面跳转

--- src/App.js
+++ src/App.js
@@ -2,12 +2,32 @@ import React, { Component } from 'react';
 import Header from './common/Header';
 import store from './store/store.js';
 import { Provider } from 'react-redux';
+import Home from './pages/Home';
+import Details from './pages/Details';
+import Diary from './pages/Diary';
+import About from './pages/About';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-          <Header /> 
+           <Router>
+            <div>
+                <Header />
+                <Route path='/' exact component={Home}></Route>
+                <Route path='/details' exact component={Details}></Route>
+                <Route path="/diary" exact component={Diary}></Route>
+                <Route path="/about" exact component={About}></Route>
+            </div>  
+           </Router>
+           <Router>
+             <div>
+                <Route path='/Login' exact component={Login}></Route>
+             </div>   
+           </Router>
       </Provider>
     );
   }
--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,36 +1,60 @@
-import React, {Component,Fragment} from 'react';
-import {Avatar,Button,Icon} from 'antd';
+import React, { Component, Fragment } from 'react';
+import { Avatar, Button, Icon } from 'antd';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Logo from './../../static/logo.jpg';
 import './header.less';
 class Header extends Component {
-    render() {
-        return (
-            <Fragment>
-                <div className='header'>
-                    <Avatar className='logo' src={Logo}/>
-                    <div className='nav'>
-                        <div className='nav-item left'><Icon type="home" theme="filled" />首页</div>
-                        <div className='nav-item left'><Icon type="book" theme="filled" />热门文章</div>
-                        <div className='nav-item left'><Icon type="heart" theme="filled" />日记</div>
-                        <div className='nav-item right'><Icon type="github" theme="filled" />关于</div>
-                    </div>
-                    <div className='inf'>
-                        <Button className='register'>注册</Button>
-                        <Button className='login' type='primary'>登陆</Button>
-                    </div>
-                </div>
-            </Fragment>
-        )
-    }
+  render() {
+    return (
+      <Fragment>
+        <div className="header">
+          <Avatar className="logo" src={Logo} />
+          <div className="nav">
+            <Link to="/">
+              <div className="nav-item left">
+                <Icon type="home" theme="filled" />
+                首页
+              </div>
+            </Link>
+            <Link to="/details">
+              <div className="nav-item left">
+                <Icon type="book" theme="filled" />
+                热门文章
+              </div>
+            </Link>
+            <Link to="/diary">
+              <div className="nav-item left">
+                <Icon type="heart" theme="filled" />
+                日记
+              </div>
+            </Link>
+            <Link to="/about">
+              <div className="nav-item right">
+                <Icon type="github" theme="filled" />
+                关于
+              </div>
+            </Link>
+          </div>
+          <div className="inf">
+            {/* <Button className="register">注册</Button> */}
+            <Link to="/Login">
+              <Button className="login" type="primary">
+                登陆
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
-const mapStateToprops=(state)=> {
+const mapStateToprops = state => {};
 
-}
+const mapDispatchToprops = dispatch => {};
 
-const mapDispatchToprops=(dispatch)=>{
-
-}
-
-export default connect(mapStateToprops,mapDispatchToprops)(Header);
+export default connect(
+  mapStateToprops,
+  mapDispatchToprops
+)(Header);
