511675e1af2829c165ff33e0e6eb86b39e9d042b
ArslanPioneer <1041238415@qq.com>
2018-12-26 00:22:47 +0800

    文章详情页面

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,11 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component,Fragment } from 'react';
 
-export default class Home extends Component {
+export default class Detail extends Component {
     render(){
         return (
-            <div>
-                Details
-            </div>
+            <Fragment>
+                 <div className='detail'>
+                    <div className='title'>人生</div>
+                    
+                    <div className='content'>
+                        <img src='https://pub-static.haozhaopian.net/static/web/site/features/cn/crop/images/crop_20a7dc7fbd29d679b456fa0f77bd9525d.jpg' />
+                        <p>哈哈哈</p>
+                        <p>哈哈哈</p>
+                        <p>哈哈哈</p>
+                        <p>哈哈哈</p>
+                        <p>哈哈哈</p>
+                        <p>哈哈哈</p>
+                        <p>哈哈哈</p>
+                        <p>哈哈哈</p>
+                        <p>哈哈哈</p>
+                        <p>哈哈哈</p>
+                        <p>哈哈哈</p>
+                        <p>哈哈哈</p>
+                        <p>哈哈哈</p>
+                        <p>哈哈哈</p>
+                        <p>哈哈哈</p>
+                        <p>哈哈哈</p>
+                        <p>哈哈哈</p>
+                    </div>
+                 </div>
+            </Fragment>
         )
     }
 }
