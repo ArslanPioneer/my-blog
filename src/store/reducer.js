064e1aaaef389cbc4d6df03d3cd8ed1f57ce045f
ArslanPioneer <1041238415@qq.com>
2018-12-29 23:29:03 +0800

    git错误修复

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 // import {reducer as headerReducer} from '../common/Header/store/reducer';
 import {reducer as homeReducer} from '../pages/Home/store';
+import {reducer as detailReducer} from '../pages/Details/store';
 const reducer =combineReducers({
     //header: headerReducer,
-    home: homeReducer
+    home: homeReducer,
+    detail:detailReducer
 })
 
 export default reducer;
